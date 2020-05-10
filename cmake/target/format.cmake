5f3cd8def0c06a11c84e4578458d25ec2314be49
Christopher Pelloux <git@chp.io>
2020-05-10 11:01:09 -0400

    Format: Use git to get the list of files
    Uses git to get tracked, untracked and cached files to feed clang-format with.
    i.e. it ignores submodule files and git ignored files.

--- cmake/target/format.cmake
+++ cmake/target/format.cmake
@@ -24,42 +24,33 @@ if(ENABLE_CLANG_FORMAT)
         format
     )
 
-    file(GLOB SUBDIRS RELATIVE ${CMAKE_SOURCE_DIR} ${CMAKE_SOURCE_DIR}/*)
-    foreach(DIR ${SUBDIRS})
-        if(IS_DIRECTORY ${CMAKE_SOURCE_DIR}/${DIR} AND NOT DIR MATCHES "^[\.].*" AND NOT DIR MATCHES "^build")
-            file(GLOB_RECURSE FILES RELATIVE ${CMAKE_BINARY_DIR} ${CMAKE_SOURCE_DIR}/${DIR}/*.h)
-            if(NOT "${FILES}" STREQUAL "")
-                add_custom_command(TARGET format
-                    COMMAND ${CMAKE_COMMAND} -E echo " - formatting c headers in: ${DIR}"
-                    COMMAND ${CMAKE_COMMAND} -E chdir ${CMAKE_BINARY_DIR} ${BF_CLANG_FORMAT} -i ${FILES}
-                )
-            endif()
+    # Find all *.h and *.cpp in git tracked files including untracked and cached files but
+    # excluding git ignored files and submodules.
+    execute_process( COMMAND
+        git ls-files --cached --others --exclude-standard -- *.hpp *.cpp
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        RESULT_VARIABLE SOURCES_RESULT
+        OUTPUT_VARIABLE SOURCES
+    )
+    if (NOT SOURCES_RESULT EQUAL 0)
+        message(FATAL_ERROR "command `git ls-files ...` returned code ${SOURCES_RESULT}")
+    endif()
 
-            file(GLOB_RECURSE FILES RELATIVE ${CMAKE_BINARY_DIR} ${CMAKE_SOURCE_DIR}/${DIR}/*.hpp)
-            if(NOT "${FILES}" STREQUAL "")
-                add_custom_command(TARGET format
-                    COMMAND ${CMAKE_COMMAND} -E echo " - formatting c++ headers in: ${DIR}"
-                    COMMAND ${CMAKE_COMMAND} -E chdir ${CMAKE_BINARY_DIR} ${BF_CLANG_FORMAT} -i ${FILES}
-                )
-            endif()
+    if(NOT "${SOURCES}" STREQUAL "")
+        string(REPLACE "\n" ";" SOURCES ${SOURCES})
 
-            file(GLOB_RECURSE FILES RELATIVE ${CMAKE_BINARY_DIR} ${CMAKE_SOURCE_DIR}/${DIR}/*.c)
-            if(NOT "${FILES}" STREQUAL "")
-                add_custom_command(TARGET format
-                    COMMAND ${CMAKE_COMMAND} -E echo " - formatting c sources in: ${DIR}"
-                    COMMAND ${CMAKE_COMMAND} -E chdir ${CMAKE_BINARY_DIR} ${BF_CLANG_FORMAT} -i ${FILES}
-                )
-            endif()
-
-            file(GLOB_RECURSE FILES RELATIVE ${CMAKE_BINARY_DIR} ${CMAKE_SOURCE_DIR}/${DIR}/*.cpp)
-            if(NOT "${FILES}" STREQUAL "")
-                add_custom_command(TARGET format
-                    COMMAND ${CMAKE_COMMAND} -E echo " - formatting c++ sources in: ${DIR}"
-                    COMMAND ${CMAKE_COMMAND} -E chdir ${CMAKE_BINARY_DIR} ${BF_CLANG_FORMAT} -i ${FILES}
-                )
-            endif()
+        list(TRANSFORM SOURCES PREPEND "${CMAKE_SOURCE_DIR}/")
+        # Transform prepend seems to add an extra element at the end
+        list(POP_BACK SOURCES EXTRA)
+        if (NOT "${EXTRA}" STREQUAL "${CMAKE_SOURCE_DIR}/")
+            message(FATAL_ERROR "${EXTRA} != ${CMAKE_SOURCE_DIR}/") #
         endif()
-    endforeach()
+
+        add_custom_command(TARGET format
+            COMMAND ${CMAKE_COMMAND} -E chdir ${CMAKE_BINARY_DIR} ${BF_CLANG_FORMAT} --verbose -i ${SOURCES}
+        )
+    endif()
+
     add_custom_command(TARGET format
         COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --green "done"
     )
